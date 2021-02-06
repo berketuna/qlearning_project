55f6e2f804e1c42f8daad63c49e5e6add0c5d79d
Berke Tuna <btuna14@ku.edu.tr>
2021-02-07 01:31:22 +0300

    added gym env

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 import numpy as np
+import gym
+
+env = gym.make("MountainCar-v0")
+env.reset()
+
+done = False
+
+while not done:
+    action = 2
+    new_state, reward, done, _ = env.step(action)
+    env.render()
+env.close()
+
+'''
+env = gym.make(env.observation_space.high)
+env = gym.make(env.observation_space.low)
+env = gym.make(env.action_space.n)
+'''
